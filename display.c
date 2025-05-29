29bdb50b40781a879e592bd2f2cc31f0e3c2e827
Divya292206 <dp062922@gmail.com>
2025-05-29 12:10:22 +0530

    Add files via upload

---
+++ display.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    printf("\n my name is \t\t: Divya");
+    printf("\n my birthdate is \t: 29/06/2005");
+    printf("\n my age is \t\t: 20");
+    printf("\n my address is \t\t: morbi");
+    
+   
+    getch();
+}
