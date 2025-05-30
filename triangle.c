94eefb8f4c68cc894deef05b4bff8cbd2e259edf
Divya292206 <dp062922@gmail.com>
2025-05-30 10:49:40 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    int base,height,ans;
+    
+    printf("enter base value :");
+    scanf("%d",&base);
+    
+    printf("enter height value :");
+    scanf("%d",&height);
+    
+    ans=(base*height)/2;
+    
+    printf("area of traingle : %d",ans);
+    
+    getch();
+    
+    
+}
