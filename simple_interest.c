aa971abd3e900c203c55e9839c64da37ef997fb5
Divya292206 <dp062922@gmail.com>
2025-05-30 11:00:58 +0530

    Add files via upload

---
+++ simple_interest.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    float p1,r1,t1,s1;
+    
+    printf("enter principal value :");
+    scanf("%f",&p1);
+    
+    printf("enter rate value :");
+    scanf("%f",&r1);
+    
+    printf("enter time :");
+    scanf("%f",&t1);
+    
+    s1=(p1*r1*t1)/100;
+    
+    printf("simple interest: %f",s1);
+    
+    getch();
+    
+    
+}
